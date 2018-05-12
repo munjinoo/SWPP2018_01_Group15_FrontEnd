93a2fac2aa422cd9526285a4ae143e792179bf80
ramyeee <ksl1817@naver.com>
2018-05-12 22:16:05 +0900

    Added Link to ClubEventPage, ClubManagePage, ClubAccountingPage

--- src/components/pages/ClubMain/index.js
+++ src/components/pages/ClubMain/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const ClubMain = () => {
   return (
     <div>
-      Club Main Page will be fixed.
+      <Link to="club/:clubid/event"> 동아리 이벤트 </Link>
+      <Link to="club/:clubid/manage"> 동아리 관리 </Link>
+      <Link to="club/:clubid/account"> 동아리 회계 관리 </Link>     
     </div>
   )
 }
