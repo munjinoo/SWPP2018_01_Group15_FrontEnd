9897517f163e2aabd505c26da08c7798ac85d03e
ramyeee <ksl1817@naver.com>
2018-05-12 22:05:26 +0900

    Added routes of ClubMain, ClubAccoutingPage, ClubEventPage, ClubManagePage

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
 import App from 'components/App'
-import { HomePage, MyPage, ClubMain, CreateClubPage, SignupPage } from 'components'
+import { HomePage, MyPage, ClubMain, CreateClubPage, SignupPage, ClubEventPage, ClubAccountingPage, ClubManagePage, ClubEventPage } from 'components'
 
 const routes = (
   <Route path="/" component={App}>
@@ -10,6 +10,9 @@ const routes = (
     <Route path = 'mypage' component={MyPage} />
     <Route path = 'club/create' component={CreateClubPage} />
     <Route path = 'club/:clubid' component={ClubMain} />
+    <Route path = 'club/:clubid/event' component = {ClubEventPage}/>
+    <Route path = 'club/:clubid/manage' component = {ClubManagePage} /> 
+    <Route path = 'club/:clubid/account' component = {ClubAccountingPage} />
     <Route path = "signup" component={SignupPage} />
   </Route>
 
