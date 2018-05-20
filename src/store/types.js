f4a5459dd338f5d3c9b296b8fab2dc5d45d09d9e
ilil96 <yh@DESKTOP-SC68LE7.localdomain>
2018-05-21 00:43:53 +0900

    Add new functions for board in types.js

--- src/store/types.js
+++ src/store/types.js
@@ -21,4 +21,26 @@ export const VERIFY_TOKEN = 'VERIFY_TOKEN'
 export const POST_CLUB = 'POST_CLUB'
 export const GET_CLUBS = 'GET_CLUBS'
 export const SET_CLUBS = 'SET_CLUBS'
+
 export const INIT_CLUB_STATE = 'INIT_CLUB_STATE'
+export const RESET_CLUBINFO = 'RESET_CLUBINFO'
+export const SET_CLUB_NEED_LOAD = 'SET_CLUB_NEED_LOAD'
+
+export const SET_CLUBNAME = 'SET_CLUBNAME'
+export const SET_CLUBID = 'SET_CLUBID'
+export const SET_CLUBADMIN = 'SET_CLUBADMIN'
+export const SET_CLUBSCOPE = 'SET_CLUBSCOPE'
+export const SET_CLUBCATEGORY = 'SET_CLUBCATEGORY'
+export const SET_CLUBINTRODUCTION = 'SET_CLUBINTRODUCTION'
+
+export const ADD_CLUBBORAD = 'ADD_CLUBBOARD'
+export const ADD_CLUBMEMBER = 'ADD_CLUBMEMBER'
+export const ADD_CLUBWAITING = 'ADD_CLUBWAITING'
+
+/* Board */
+export const GET_BOARD = 'GET_BOARD'
+export const POST_BOARD = 'POST_BOARD'
+export const INIT_BOARD_STATE = 'INIT_BOARD_STATE'
+
+
+
