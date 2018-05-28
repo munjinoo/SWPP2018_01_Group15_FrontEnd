b17c27accc6dbb288c5bcc03c212dcf05e88b682
ilil96 <yh@DESKTOP-SC68LE7.localdomain>
2018-05-29 02:26:25 +0900

    Add delete button next to each board

--- src/components/organisms/BoardList/index.js
+++ src/components/organisms/BoardList/index.js
@@ -8,24 +8,30 @@ const Wrapper = styled.div`
   color: ${palette('grayscale', 0)};
 `
 
-const BoardList = ({ clubState={id, boards: [], needLoading: true}, onLoad, clubid}) => {
+const BoardList = ({ clubState={id, boards: [], needLoading: true}, onLoad, clubid, onDeleteBoard}) => {
   const loadingFunc = () => {
     onLoad(clubid)
   }
-
+  
   if (clubState.needLoading)
     return(
       <Wrapper>
         <Load onLoad={loadingFunc} />
       </Wrapper>
     )
+    
+
+  const onClick = (e) => {
+    onDeleteBoard(e.target.id)
+    window.location.reload() // 새로고침
+  }
   return (    
     <Wrapper>
       전체 게시판  <br />
       <ul>
         {clubState.boards.map((board) =>
           <li key={board.id}>
-            {board.name}
+            {board.name} <button onClick={onClick} id={board.id}> 삭제 </button>
           </li>
         )}
       </ul>
