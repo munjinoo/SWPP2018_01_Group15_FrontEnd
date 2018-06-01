084e1b61de3cdcd201f414c8ef4614c664634e2f
Unknown <njs03332@gmail.com>
2018-06-02 01:00:20 +0900

    comments for some fields in club selectors

--- src/store/club/selectors.js
+++ src/store/club/selectors.js
@@ -2,3 +2,7 @@ export const clubState = { admin: [], members: [], waitings: [],
     id: null, name: null, scope: null, 
     category: null, introduction: null, 
     boards: [], future_events: [], past_events: [], needLoading: true }
+
+    // admin, members, waitings: {id: ~, name: ~}
+    // boards: {id: ~, name: ~}
+    // future_events, past_events: id
