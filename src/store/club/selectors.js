6528efaf9472dd27ba5be5d2a1cfb8071a44303b
Unknown <njs03332@gmail.com>
2018-06-02 18:29:18 +0900

    delete needLoading, add comment

--- src/store/club/selectors.js
+++ src/store/club/selectors.js
@@ -1,8 +1,10 @@
 export const clubState = { admin: [], members: [], waitings: [], 
     id: null, name: null, scope: null, 
     category: null, introduction: null, 
-    boards: [], future_events: [], past_events: [], needLoading: true }
+    boards: [], future_events: [], past_events: [] }
 
     // admin, members, waitings: {id: ~, name: ~}
     // boards: {id: ~, name: ~}
     // future_events, past_events: id
+
+    // members includes admin
