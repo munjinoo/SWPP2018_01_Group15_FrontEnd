dfbf115599dfb643776c6f7376d454a7856853a6
Unknown <njs03332@gmail.com>
2018-06-02 18:28:54 +0900

    handle showing clubs as member only

--- src/components/organisms/ClubList/index.js
+++ src/components/organisms/ClubList/index.js
@@ -3,25 +3,43 @@ import styled from 'styled-components'
 import { font, palette } from 'styled-theme'
 import { Link } from 'react-router'
 import { Load } from 'components'
+import lifecycle from 'react-pure-lifecycle'
 
 const Wrapper = styled.div`
   font-family: ${font('primary')};
   color: ${palette('grayscale', 0)};
 `
 
+const componentDidMount = (props) => {
+  props.onLoad()
+}
+
+const methods = {
+  componentDidMount
+}
+
 const ClubList = ({ userState = { clubs_as_admin: [], clubs_as_member: [], needLoading: {club: true}}, onLoad }) => {
-  if (userState.needLoading.club)
-    return (
-      <Wrapper>
-        <Load onLoad={onLoad} />
-      </Wrapper>
-    )
+  
+  
+  
   console.log(userState);
+  const as_member = userState.clubs_as_member
+  const as_admin = userState.clubs_as_admin
+  let only_as_member = []
+  console.log("as_member", as_member)
+  console.log("as_admin", as_admin)
+  for (var club in as_member) {
+    if (!as_admin.hasOwnProperty(club)) {
+      only_as_member.add(club)
+    }
+  }
+  console.log("only_as_member", only_as_member)
+
   return (
     <Wrapper>
       내가 관리하는 동아리 <br />
       <ul>
-        {userState.clubs_as_admin.map(club =>
+        {as_admin.map(club =>
           <li key={club.id}>
             <Link to={`/club/${club.id}`}>{club.name}</Link>
           </li>
@@ -29,7 +47,7 @@ const ClubList = ({ userState = { clubs_as_admin: [], clubs_as_member: [], needL
       </ul>
       내가 가입한 동아리<br />
       <ul>
-        {userState.clubs_as_member.map(club =>
+        {only_as_member.map(club =>
           <li key={club.id}>
             <Link to={`/club/${club.id}`}>{club.name}</Link>
           </li>
@@ -39,5 +57,4 @@ const ClubList = ({ userState = { clubs_as_admin: [], clubs_as_member: [], needL
   )
 }
 
-
-export default ClubList
+export default lifecycle(methods)(ClubList)
