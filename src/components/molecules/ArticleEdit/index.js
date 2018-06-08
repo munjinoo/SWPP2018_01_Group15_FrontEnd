3268017ebf9708a26ae5c235d22d9bffac0f1bdf
ilil96 <37105138+ilil96@users.noreply.github.com>
2018-06-08 21:34:15 +0900

    Manage
    * article page modified
    
    * Modify articlepage
    
    * Move articledetial from molecule to organism

---
+++ src/components/molecules/ArticleEdit/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+import { font, palette } from 'styled-theme'
+
+const Wrapper = styled.div`
+  font-family: ${font('primary')};
+  color: ${palette('grayscale', 0)};
+`
+
+const ArticleEdit = ({ articleid, title, content, onPutArticle }) => {
+  let new_title='';
+  let new_content='';
+  const onClick = () => {
+    onPutArticle(articleid, new_title.value, new_content.value);
+    window.location.reload();
+  }
+  return (
+    <Wrapper>
+      제목: <input type="text" defaultValue={title} ref={node => {new_title=node;}} /> <br/>
+      <input type="text" defaultValue={content} ref={node => {new_content=node;}} /> <br/>
+      <button onClick={onClick}>수정</button>      
+    </Wrapper>
+  )
+}
+
+
+export default ArticleEdit
